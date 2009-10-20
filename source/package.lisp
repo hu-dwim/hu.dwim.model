9908a91945375a45de556bc01d4000fd34a77f2a
levente.meszaros <levente.meszaros@gmail.com>
2009-10-20 14:09:47 +0000

    Follow hu.dwim.common-lisp renaming.

--- source/package.lisp
+++ source/package.lisp
@@ -8,7 +8,7 @@
 
 (defpackage :hu.dwim.model
   (:use :hu.dwim.asdf
-        :hu.dwim.common-lisp
+        :hu.dwim.common
         :hu.dwim.def
         :hu.dwim.defclass-star
         :hu.dwim.logger
--- test/package.lisp
+++ test/package.lisp
@@ -7,7 +7,7 @@
 (in-package :common-lisp-user)
 
 (defpackage :hu.dwim.model.test
-  (:use :hu.dwim.common-lisp
+  (:use :hu.dwim.common
         :hu.dwim.def
         :hu.dwim.defclass-star
         :hu.dwim.model
